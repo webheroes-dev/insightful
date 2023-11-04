0221db832110021c16a4d4f0ae1491828d561bd9
Krystian Sowiński <88927709+plumcoding@users.noreply.github.com>
2023-11-04 12:45:52 +0100

    Revert "chore: deploy to github pages"
    This reverts commit ddee0a039673e2f0375954c739d869d5b82a294b.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'webheroes-dev', // Usually your GitHub org/user name.
-  projectName: 'insightful', // Usually your repo name.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
